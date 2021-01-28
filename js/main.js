688c71071bafc5bdb46e0f2de7901955a4a3c505
Laura Portillo <lauraportillo.ark@gmail.com>
2021-01-28 08:50:39 +0100

    start the exercise

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// const ourDiv = document.querySelector(".js-background");
+
+// function setBg() {
+//   console.log("me ejecuto");
+//   if (window.scrollY > 200) {
+//     ourDiv.classList.remove("background1");
+//     ourDiv.classList.add("background2");
+//   } else {
+//     ourDiv.classList.add("background1");
+//     ourDiv.classList.remove("background2");
+//   }
+// }
+// window.addEventListener("scroll", setBg);

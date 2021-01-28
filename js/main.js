49929017f45fce338e996db121401cbefb24aadd
Laura Portillo <lauraportillo.ark@gmail.com>
2021-01-28 11:25:23 +0100

    add final exercise

--- js/main.js
+++ js/main.js
@@ -5,32 +5,24 @@ const tryElement = document.querySelector('.js-try');
 const clueElement = document.querySelector('.js-clue');
 const attempsElement = document.querySelector('.js-attemps');
 
-// generar un número random para iniciar el juego
 const result = getRandomNumber(100);
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 console.log(result);
 
-// escuchar el botón prueba
 function startGame() {
-  const gameNumber = guessElement.value;
+  const gameNumber = parseInt(guessElement.value);
   console.log(gameNumber);
 
-  //escribir las condiciones para el numero que haya introducido la usuaria
-
   if (gameNumber === result) {
-    clueElement.innerHTML = ' Has ganado campeona!!';
+    clueElement.value = ' Has ganado campeona!!';
   } else if (gameNumber > result) {
-    clueElement.innerHTML = ' Demasiado alto';
+    clueElement.value = ' Demasiado alto';
   } else if (gameNumber < result) {
-    clueElement.innerHTML = ' Demasiado bajo';
+    clueElement.value = ' Demasiado bajo';
   } else {
-    clueElement.innerHTML = ' El número debe estar entre 1 y 100';
+    clueElement.value = ' El número debe estar entre 1 y 100';
   }
 }
 tryElement.addEventListener('click', startGame);
-
-/*
-  Actualizar el contador de intentos cada vez que la jugadora pruebe
-  */

75d4603bd478cbdf85f7b06fdf9ef7d23c755da9
Laura Portillo <lauraportillo.ark@gmail.com>
2021-02-01 22:48:42 +0100

    add improvements to the game

--- js/main.js
+++ js/main.js
@@ -1,28 +1,49 @@
 'use strict';
+// variables
 
 const guessElement = document.querySelector('.js-guess');
 const tryElement = document.querySelector('.js-try');
 const clueElement = document.querySelector('.js-clue');
 const attempsElement = document.querySelector('.js-attemps');
+let keystrokes = 0;
 
 const result = getRandomNumber(100);
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-console.log(result);
 
-function startGame() {
+// funciones
+function handleStartGame() {
   const gameNumber = parseInt(guessElement.value);
-  console.log(gameNumber);
 
-  if (gameNumber === result) {
-    clueElement.value = ' Has ganado campeona!!';
+  if (isNaN(gameNumber)) {
+    clueElement.value = ' Te has liaoooo! Te he dicho un número...';
+    clueElement.classList.add('clueTextColorError');
+  } else if (gameNumber < 1 || gameNumber > 100) {
+    clueElement.value = ' Dijimos que eligieras entre 1 y 100! Sin trampis ;)';
+    clueElement.classList.add('clueTextColorError');
   } else if (gameNumber > result) {
     clueElement.value = ' Demasiado alto';
+    clueElement.classList.add('clueTextColorError');
   } else if (gameNumber < result) {
     clueElement.value = ' Demasiado bajo';
-  } else {
-    clueElement.value = ' El número debe estar entre 1 y 100';
+    clueElement.classList.add('clueTextColorError');
+  } else if (gameNumber === result) {
+    clueElement.value = ' Has ganado!! Oleeee, oleeeee y oleeeee';
+    clueElement.classList.add('clueTextColor');
+    clueElement.classList.remove('clueTextColorError');
+    //STOP GAME
+    tryElement.disabled = true;
+    guessElement.disabled = true;
   }
 }
-tryElement.addEventListener('click', startGame);
+
+function handleCountAttemps() {
+  keystrokes = keystrokes + 1;
+  attempsElement.value = 'Número de intentos: ' + keystrokes;
+}
+
+// EJECUCION: se ejecuta al cargar
+tryElement.addEventListener('click', handleStartGame);
+tryElement.addEventListener('click', handleCountAttemps);
+console.log(result);
